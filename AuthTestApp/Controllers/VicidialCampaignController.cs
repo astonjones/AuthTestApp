874620dff7ecac9ba1ffe9dc49658dfc08331b8a
Aston Jones <astonbradleyjones@gmail.com>
2021-11-19 17:13:43 -0600

    Added mvc for ViciDial Campaign. Pushing to pull from laptop to deal with errors.

--- AuthTestApp/Controllers/VicidialCampaignController.cs
+++ AuthTestApp/Controllers/VicidialCampaignController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collection.Generic;
+using System;
 
 namespace AuthTestApp.Controllers
 {
     public class VicidialCampaignController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public VicidialCampaignController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Ticket> objList = _db.Ticket;
+            return View(objList);
+        }
+
+        //GET - Edit
+        public IActionResult Details(string? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Ticket.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
         }
     }
 }
